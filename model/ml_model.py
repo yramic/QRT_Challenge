8e6ab2a2b01fb1fd5adb887dda962e340b98e0c6
yramic <yannick.ramic@outlook.com>
2025-01-31 15:16:58 +0100

    Put together allused models

--- model/ml_model.py
+++ model/ml_model.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
+from typing import Union
+from sklearn.base import BaseEstimator
+from torch.nn import Module as TorchModule
 
 class ModelTrainer:
-    def __init__(self, model, n_splits=4, random_state=0):
+    def __init__(
+            self, 
+            model: Union[BaseEstimator, TorchModule], 
+            use_model: str, 
+            n_splits: int = 4, 
+            random_state: int = 0
+        ) -> None:
+        """Initialize Model Trainer"""
         self.model = model
+        self.use_model = use_model
         self.n_splits = n_splits
         self.random_state = random_state
         self.models = []
         self.scores = []
 
-    def cross_validate(self, X, Y):
+    def cross_validate(self, X: pd.DataFrame, Y: pd.DataFrame) -> None:
         """
         Perform standard K-fold cross-validation without using date-based splits.
         """
@@ -41,6 +52,44 @@ class ModelTrainer:
         std_score = np.std(self.scores) * 100
         print(f"Cross-validation Accuracy: {mean_score:.2f}% ± {std_score:.2f}%")
 
+    
+    def validate_model(
+            self, 
+            X: pd.DataFrame, 
+            Y: pd.DataFrame,
+            time_limit: int = 3600, # 1 hour time limit for training
+            num_gpus: int = 1,
+            num_cpus: int = 1
+        ) -> None:
+        """For some NN models or something like AutoGluon Cross Validation can't be used
+        thus the model's performance can be evaluated by a simple train /test split."""
+
+        assert self.use_model == 'AutoGluon', (
+            "This validation is only valid for AutoGluon; else use Cross Validation"
+        )
+
+        X = pd.concat((X, Y), axis=1)
+
+        X_train, X_eval = train_test_split(
+            X,  
+            test_size=0.2,  
+            random_state=42, 
+            stratify=X["RET"]
+        )
+
+        self.model.fit(
+            train_data=X_train,
+            time_limit=time_limit, 
+            verbosity=3,
+            num_gpus=num_gpus,
+            num_cpus=num_cpus
+        )
+
+        prediction = self.model.predict_proba(X_eval)[True]
+        Y_pred = prediction.transform(
+            lambda x: x > x.median()).values
+        
+        print('MEDIAN', accuracy_score(Y_pred, X_eval['RET']))
 
 
     def predict(self, X, group_col=None):
@@ -49,7 +98,10 @@ class ModelTrainer:
         """
         if group_col:
             X_copy = X.copy()
-            X_copy['pred'] = self.model.predict_proba(X.drop(columns=[group_col]))[:, 1]
+            if self.use_model != 'AutoGluon':
+                X_copy['pred'] = self.model.predict_proba(X.drop(columns=[group_col]))[:, 1]
+            elif self.use_model == 'AutoGluon':
+                X_copy['pred'] = self.model.predict_proba(X.drop(columns=[group_col]))[True]
             return X_copy.groupby(group_col)['pred'].transform(lambda x: x > x.median()).astype(int)
         return self.model.predict(X)
 
